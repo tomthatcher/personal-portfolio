c3890fba79909ac085eac95652af491c566fbf5d
tomthatcher <tomthatcherr@gmail.com>
2026-07-08 00:31:27 +1000

    Initial commit: Deploying Astro architecture

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Change 'your-username' and 'your-repo-name' to match your GitHub profile details
+  site: 'https://tomthatcher.github.io',
+  // base: '/personal-portfolio',
+});
